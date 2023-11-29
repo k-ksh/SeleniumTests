cfe01031475046075134260b67f826d19e74f40b
Kateryna Kateryna <katya.shvachko@gmail.com>
2023-11-29 17:10:23 +0200

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Infopulse LLC")]
+[assembly: AssemblyProduct("SeleniumTests")]
+[assembly: AssemblyCopyright("Copyright © Infopulse LLC 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e709fbf-0fbe-4236-94b4-cdb4934830ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class DropdownTest
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void VerifyNumberOfItemsInDropdown()
+        {
+          
+            driver.Navigate().GoToUrl("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+
+           
+            IWebElement dropdown = driver.FindElement(By.CssSelector("#post-2646 > div.twelve.columns > div > div > div > p > select"));
+
+           
+            var options = dropdown.FindElements(By.TagName("option"));
+
+            
+            Assert.AreEqual(249, options.Count, "Number of items in the dropdown is not equal to 249");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            
+            driver.Quit();
+        }
+    }
+}
